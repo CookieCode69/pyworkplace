c444304ecacce873774d8fcfc251234eb35de85d
Cookie Code <cookiecode69@gmail.com>
2020-01-11 13:01:51 +0700

    using recursion for fibonaci

--- fibonaci.py
+++ fibonaci.py
@@ -1,21 +1,26 @@
 inputNum = int(input("Type a number: "))
+x = 1
+y = 0
+index = 0
 
 
-def fibonaci(num):
-        x = 1
-        y = 0
-        index = 0
+def fibonaci(num,x,y,index):
         if num <= 0:
                 print("invalid value!")
         elif num == 1:
-                print(1)
+                print(num)
         else:
-                while x <= num:
-                        print(x)
-                        index = x
-                        x = x + y
-                        y = index
+                print(x)
+                index = x
+                x = x + y
+                y = index
+                if x == num:
+                        return x
+                elif x > num:
+                        return y
+                else:
+                        fibonaci(num,x,y,index)
 
-fibonaci(inputNum)
+fibonaci(inputNum,x,y,index)
                 
                 
