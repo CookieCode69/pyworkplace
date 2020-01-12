c34b15849fa83fa2fe3c989908367362125f1d4b
Cookie Code <cookiecode69@gmail.com>
2020-01-12 10:38:13 +0700

    working with strings :D

--- fibonaci.py
+++ fibonaci.py
@@ -1,26 +1,28 @@
-inputNum = int(input("Type a number: "))
-x = 1
-y = 0
-index = 0
+INPUT_NUM = int(input("Type a number: "))
+X = 1
+Y = 0
+INDEX = 0
 
 
-def fibonaci(num,x,y,index):
-        if num <= 0:
-                print("invalid value!")
-        elif num == 1:
-                print(num)
+def fibonaci(num):
+    global X
+    global Y
+    global INDEX
+
+    if num <= 0:
+        print("invalid value!")
+    elif num == 1:
+        print(num)
+    else:
+        print(X)
+        INDEX = X
+        X = X + Y
+        Y = INDEX
+        if X == num:
+            return X
+        elif X > num:
+            return Y
         else:
-                print(x)
-                index = x
-                x = x + y
-                y = index
-                if x == num:
-                        return x
-                elif x > num:
-                        return y
-                else:
-                        fibonaci(num,x,y,index)
+            fibonaci(num)
 
-fibonaci(inputNum,x,y,index)
-                
-                
+fibonaci(INPUT_NUM)
---
+++ pystrings.py
@@ -0,0 +1,4 @@
+foo = "nice"
+f = foo[0:2]
+oo = foo[2:4]
+print(f + oo)
